f4ec0ccaab1eb5071e9021f271923d966713cbf4
Damilare <bolarinwaoluwadamilare91@gmail.com>
2024-08-07 06:22:42 +0100

    Create The Side Bar

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SideBar from "@/components/SideBar";
 export default function Home() {
   return (
     <main className="grid grid-cols-6 grid-rows-12">
-      <aside className="row-span-10 bg-[#000066] ">
+      <aside className="row-span-10 w-[270px] h-[1024px] bg-[#000066]">
         <SideBar />
       </aside>
       <header className="col-span-5 h-[96px]">
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,20 +1,80 @@
-import Image from "next/image";
+"use client";
 import logo from "@/public/pistis Logo_03 1.png";
-import teacher from "@/public/teacher.png";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { BiCategory } from "react-icons/bi";
+import { GiTeacher } from "react-icons/gi";
+import { IoSettingsOutline } from "react-icons/io5";
+import { SlLogout } from "react-icons/sl";
+import { TbAward } from "react-icons/tb";
+import { VscBook } from "react-icons/vsc";
 
 export default function SideBar() {
+  const pathname = usePathname();
   return (
-    <div className="p-4">
-      <Image src={logo} alt="Pistis Logo" width={188} height={45.15} />
-      <div className="w-[270px] h-[834.85px] mt-4 p-2.5 text-white">
-        <Image
-          src={teacher}
-          alt="Teacher"
-          width={26}
-          height={26}
-          color="#f1f1f1"
-        />
-        <h1>This is my Side Bar</h1>
+    <div>
+      {/* Top Header */}
+      <div className="p-4 mb-4">
+        <Image src={logo} alt="Logo" width={188} height={45.15}></Image>
+      </div>
+      {/* Main Nav Links */}
+      <ul className="pt-2">
+        <li
+          className={`flex ${
+            pathname === "/" && "bg-[#33cc99] text-black"
+          } font-serif w-full p-4 text-special items-center cursor-pointer text-[#f1f1f1] hover:bg-[#33cc99]`}
+        >
+          <Link href={"/"}>
+            <BiCategory size={30} className="block float-left mr-4" />
+            Dashbroad
+          </Link>
+        </li>
+        <li
+          className={`flex ${
+            pathname === "/Course_Content" && "bg-[#33cc99]"
+          } font-serif w-full p-4 text-special items-center cursor-pointer text-[#f1f1f1] hover:bg-[#33cc99]`}
+        >
+          <Link href={"/Course_Content"}>
+            <VscBook size={30} className="block float-left mr-4" />
+            Course Content
+          </Link>
+        </li>
+        <li
+          className={`flex ${
+            pathname === "/Projects" && "bg-[#33cc99]"
+          } font-serif w-full p-4 text-special items-center cursor-pointer text-[#f1f1f1] hover:bg-[#33cc99]`}
+        >
+          <Link href={"/Projects"}>
+            <GiTeacher size={30} className="block float-left mr-4" />
+            Projects
+          </Link>
+        </li>
+        <li
+          className={`flex ${
+            pathname === "/Grading" && "bg-[#33cc99] text-black"
+          } font-serif w-full p-4 text-special items-center cursor-pointer text-[#f1f1f1] hover:bg-[#33cc99]`}
+        >
+          <Link href={"/Grading"}>
+            <TbAward size={30} className="block float-left mr-4" />
+            Grading
+          </Link>
+        </li>
+        <li
+          className={`flex ${
+            pathname === "/Settings" && "bg-[#33cc99]"
+          } font-serif w-full p-4 text-special items-center cursor-pointer text-[#f1f1f1] hover:bg-[#33cc99]`}
+        >
+          <Link href={"/Settings"}>
+            <IoSettingsOutline size={30} className="block float-left mr-4" />
+            Settings
+          </Link>
+        </li>
+      </ul>
+      {/* Footer */}
+      <div className="mt-96 flex font-serif w-full p-4 text-special items-center cursor-pointer text-[#f1f1f1] hover:bg-[#33cc99]">
+        <SlLogout size={30} className="block float-left mr-4" />
+        <span>Log Out</span>
       </div>
     </div>
   );
