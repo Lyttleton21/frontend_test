a1faefc2caeb8fe997bf33e160731deacbb167a2
Damilare <bolarinwaoluwadamilare91@gmail.com>
2024-08-11 21:29:05 +0100

    Add rotate div to the welcome component

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -6,7 +6,7 @@ export default function MainContent() {
   return (
     <div className="lg:col-span-3 xl:m-3">
       <div className="lg:grid lg:grid-cols-3 lg:p-2 xl:p-0">
-        <div className="p-4 text-center bg-[#ffffff] lg:h-[133px] lg:text-left lg:col-span-3 lg:row-span-1 lg:rounded-lg lg:pl-6 lg:mb-2 xl:mb-0">
+        <div className=" text-center bg-[#ffffff] lg:h-[133px] lg:text-left lg:col-span-3 lg:rounded-lg lg:pl-6 lg:mb-2 xl:mb-0">
           <Welcome />
         </div>
         <div className=" lg:col-span-3 lg:mb-2 xl:mb-0">
--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
+import hand from "@/public/🦆 emoji _flexed biceps_.png";
 export default function Welcome() {
   return (
-    <div className="mt-5 lg:p-2 lg:pl-3 ">
-      <h3 className="font-serif text-2xl font-medium leading-8">
-        Welcome Beatrice
-      </h3>
-      <small className="font-serif text-base font-normal leadind-[19.5px]">
-        Complete your course and take a step further
-      </small>
+    <div className="w-full h-full lg:flex lg:overflow-hidden">
+      <div className="w-full p-6 lg:p-0 lg:self-center">
+        <h3 className="font-serif text-2xl font-medium leading-8">
+          Welcome Beatrice
+        </h3>
+        <small className="font-serif text-base font-normal leadind-[19.5px] xl:mr-1">
+          Complete your course and take a step further👋
+        </small>
+      </div>
+      <div className="hidden relative w-full h-full  lg:block">
+        <div className="absolute z-50 right-[-20px] top-[-30px] w-[135.58px] h-[119.44px] rounded-lg bg-[#33CC99] rotate-45"></div>
+        <div className="absolute z-0 right-[-85px] bottom-[-50px] w-[191.69px] h-[134.64px] rounded-lg bg-[#000066] rotate-45"></div>
+      </div>
     </div>
   );
 }
